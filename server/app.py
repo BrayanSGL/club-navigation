5d8b9f6c0e64960d131ae0e0bdf8dcdf5db7e3f9
sebastian2000pk <juochoas@unal.edu.co>
2024-10-18 18:17:43 -0500

    clock

--- server/app.py
+++ server/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-
-    return "Hello World!"
+@app.route('/clock')
+def clock():
+    now = datetime.now()
+    start_of_day = datetime(now.year, now.month, now.day)
+    milliseconds_since_start_of_day = (now - start_of_day).total_seconds() * 1000
+    new_time = milliseconds_since_start_of_day * 48
+    new_date = start_of_day + timedelta(milliseconds=new_time)
+    return new_date.strftime('%Y-%m-%d %H:%M:%S')
+    
   
 if __name__ == '__main__':
-    app.run(host="0.0.0.0" , port=5000)
+    app.run(host="0.0.0.0" , port=5000) 
