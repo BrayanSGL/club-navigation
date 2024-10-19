6e863bffbc9f1286385ed0ca12969a0cc54060c6
BrayanSGL <bsgaleanol@udistrital.edu.co>
2024-10-19 16:48:14 -0500

    updatess

--- server/config.py
+++ server/config.py
@@ -7,3 +7,4 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    print(os.getenv("DB_USER"),"*******************************MIERDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA****************************")
