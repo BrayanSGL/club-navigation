a1ec74752be4e415ab1d9e8d710843335e29bf10
sebastian2000pk <juochoas@unal.edu.co>
2024-11-02 08:35:16 -0500

    test fix

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
-from .routes import register_routes
-from .models.seed import seed_data
 
 # Inicializa SQLAlchemy
 db = SQLAlchemy()
@@ -13,14 +11,17 @@ def create_app():
     
     # Inicializa la base de datos con la aplicación
     db.init_app(app)
-    
-    # Registra las rutas en la aplicación
-    register_routes(app)
 
     with app.app_context():
         # Crea las tablas en la base de datos si no existen
         db.create_all()
+        
+        from .models.seed import seed_data
         seed_data()
+
+        # Registra las rutas en la aplicación
+        from .routes import register_routes
+        register_routes(app)
         
 
     return app
