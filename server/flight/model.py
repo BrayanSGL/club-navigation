6397d1a03256915c6ffc18ba1fcf42e92dc56066
sebastian2000pk <juochoas@unal.edu.co>
2024-11-02 09:15:45 -0500

    fix

--- server/flight/model.py
+++ server/flight/model.py
@@ -10,3 +10,14 @@ class Flight():
 
   def __repr__(self):
     return f"<Fligth {self.id}>"
+
+  def to_json(self):
+    return {
+      "id": self.id,
+      "origin": self.origin,
+      "destination": self.destination,
+      "departure": self.departure,
+      "arrival": self.arrival,
+      "duration": self.duration,
+      "waiting_time": self.waiting_time
+    }
