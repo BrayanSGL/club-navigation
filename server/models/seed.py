d4fae43f60999f00581cba80b1e146dd6462cf8c
sebastian2000pk <juochoas@unal.edu.co>
2024-11-02 08:38:18 -0500

    tewst

--- server/models/seed.py
+++ server/models/seed.py
@@ -6,13 +6,13 @@ def seed_data():
   # Datos por defecto
   default_flights = [
     Flights(
-      origin=flight['origin'], 
-      destination=flight['destination'], 
+      origin=flight['code'], 
+      destination=FLIGHTS[index + 1]['code'], 
       duration=flight['duration'], 
       departure=flight['departure'], 
       arrival=flight['arrival'], 
       waiting_time=flight['waiting_time'])
-    for flight in FLIGHTS
+    for flight, index in FLIGHTS
   ]
 
   # Agregar los productos si no existen
