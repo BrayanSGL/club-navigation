dc6ffac297d76840d0ca9d3756ac993b0b1749be
sebastian2000pk <juochoas@unal.edu.co>
2024-11-02 22:05:28 -0500

    test

--- server/models/User.py
+++ server/models/User.py
@@ -7,7 +7,7 @@ class User(db.Model):
   email = db.Column(db.String(100), nullable=False)
   password = db.Column(db.String(100), nullable=False)
   
-  flights = db.relationship('UserFlight', backref='user', lazy=True)
+  flights = db.relationship('UserFlight', back_populates='user')
 
   def __repr__(self):
     return f"<User {self.username}>"
