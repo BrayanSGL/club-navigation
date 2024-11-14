0946eca998fa127d345536109bc228e7b0839116
sebastian2000pk <juochoas@unal.edu.co>
2024-11-13 19:53:10 -0500

    test

--- server/models/UserFlight.py
+++ server/models/UserFlight.py
@@ -7,6 +7,7 @@ class UserFlight(db.Model):
   flight_id = db.Column(db.Integer, db.ForeignKey('flights.id'), nullable=False)
   
   flight = db.relationship('Flight', back_populates='users')
+  user = db.relationship('User', back_populates='flights')
 
   def __repr__(self):
     return f"<UserFlight {self.id}>"
