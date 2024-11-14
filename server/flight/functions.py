d66025519250101b3cd1f381125e18d66252c549
sebastian2000pk <juochoas@unal.edu.co>
2024-11-13 20:39:33 -0500

    tset

--- server/flight/functions.py
+++ server/flight/functions.py
@@ -1,21 +1,28 @@
 
 from .consts import DESTINATIONS
 from .model import Flight
+from datetime import datetime, timedelta
 
 def generate_schedule():
   schedule = []
+  initial_date = datetime.now()
+  date_departure = initial_date
+  
   for i in range(len(DESTINATIONS)):
     if i == len(DESTINATIONS) - 1:
       break
     flight = DESTINATIONS[i]
     next_flight = DESTINATIONS[i + 1]
+    
     schedule.append(Flight(
       id=i + 1,
-      origin=flight["code"],
-      destination=next_flight["code"],
-      departure=flight["departure"],
-      arrival=next_flight["departure"],
-      duration=flight["flightTime"],
+      origin_code=flight["code"],
+      origen=flight["name"],
+      destination=next_flight["name"],
+      destination_code=next_flight["code"],
+      departure=date_departure,
+      arrival=date_departure + timedelta(minutes=flight["totalTime"]),
+      duration=flight["totalTime"],
     ))
     
   return [flight.to_json() for flight in schedule]
--- server/flight/model.py
+++ server/flight/model.py
@@ -1,8 +1,10 @@
 class Flight():
-  def __init__(self, id, origin, destination, departure, arrival, duration):
+  def __init__(self, id, origin_code, origen, destination, destination_code, departure, arrival, duration):
     self.id = id
-    self.origin = origin
+    self.origin_code = origin_code
+    self.origin = origen
     self.destination = destination
+    self.destination_code = destination_code
     self.departure = departure
     self.arrival = arrival
     self.duration = duration
@@ -13,8 +15,10 @@ class Flight():
   def to_json(self):
     return {
       "id": self.id,
+      "origin_code": self.origin_code,
       "origin": self.origin,
       "destination": self.destination,
+      "destination_code": self.destination_code,
       "departure": self.departure,
       "arrival": self.arrival,
       "duration": self.duration,
